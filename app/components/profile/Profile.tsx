bfcd9b8e82ed2a2a20933e9218497782aca07c6c
Felix Mokross <felix.mokross@zuehlke.com>
2023-09-18 20:09:54 +0200

    Implement profile form

--- app/components/profile/Profile.tsx
+++ app/components/profile/Profile.tsx
@@ -1,3 +1,22 @@
+import { Form } from "@remix-run/react";
+
 export function Profile() {
-  return <p>To be implemented</p>;
+  return (
+    <Form method="post">
+      <div>
+        <label htmlFor="name">Name: </label>
+        <input type="text" id="name" name="name" placeholder="Dr. React" />
+      </div>
+      <div>
+        <label htmlFor="email">E-Mail: </label>
+        <input
+          type="text"
+          placeholder="react@zuhlke.com"
+          id="email"
+          name="email"
+        />
+      </div>
+      <input type="submit" value="save" />
+    </Form>
+  );
 }
